1dd1e4eaaf4b171bd13d1d637c34624bb48204f6
zion <zion4axas@gmail.com>
2024-08-02 10:25:29 +0300

    lifespan

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ faststream docs serve backend.main:app
 
 durable - свойство назначается для каждого элемента отдельно:
 exch, pub, sub, message=persist
+
+
+.env:
+1. ENV=.test.env faststream run serve:app
+в config создается объект settings = Settings(_env_file=os.getenv("ENV", ".env"))
+2. faststream run backend.main:app --env .env.test
+в параметры lifespan передается аргумент env
+в lifespan создается объект settings = Settings(_env_file=env)
+объект помещается в контекст context.set_global("settings", settings)
--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,16 @@
-from faststream import FastStream
+import logging
+
+from faststream import ContextRepo, FastStream
 from faststream.rabbit import RabbitBroker
 
-from backend.handler import router as router_handler
 from backend.context import router as router_context
-from config import configure_logging, settings
+from backend.handler import router as router_handler
+from config import Settings, configure_logging, settings
 
 configure_logging()
 
 
-broker = RabbitBroker(settings.BROKER)
+broker = RabbitBroker(settings.BROKER, log_level=logging.DEBUG)
 # apply_types=False  - decorator disable Pydantic validation, Depends, Context
 # validate=False  - disable validation
 
@@ -18,6 +20,38 @@ broker.include_router(router=router_context)
 app = FastStream(broker)
 
 
+# faststream run backend.main:app --env .env.test
+# the env field will be passed to the setup function from the args with the command line
+# Command line args are available in all @app.on_startup hooks.
+# To use them in other parts of the application, put them in the ContextRepo
+@app.on_startup
+async def setup(context: ContextRepo, env: str = ".env"):  # env from --env
+    settings = Settings(_env_file=env)
+    context.set_global("settings", settings)
+    # await broker.connect(settings.BROKER)  # init broker if: broker = RabbitBroker()
+
+
 @app.after_startup
 async def test():
     await broker.publish("Hello", queue="que5")
+
+
+# hooks:
+# @app.on_startup           called BEFORE the broker is launched
+# @app.after_startup        AFTER initializing the broker
+# @app.on_shutdown          called BEFORE the broker is stopped
+# @app.after_shutdown       triggered AFTER stopping the broker
+# OR
+# instead @app.on_startup & @app.after_shutdown
+"""
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(context: ContextRepo, env: str = ".env"):
+    # BEFORE the broker is launched
+    settings = Settings(_env_file=env)
+    context.set_global("settings", settings)
+    
+    yield
+    # AFTER stopping the broker
+"""
